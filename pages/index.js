4ebf4d7a780dd7e786e3abfe2f25f1b1fc5afd27
Parshvi <parshvisri@gmail.com>
2022-05-11 17:51:49 +0530

    adding abi json files

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
 import styles from '../styles/Home.module.css';
-import WhiteList from '../artifacts/contracts/WhiteList.sol/WhiteList.json';
+import WhiteList from '../utils/WhiteList.json';
 
 export default function Home() {
   const[user,setUser] = useState('');
